7aba5e91aee0ed0f1019a7aad56cdada3a98c473
Ronish <ronish.thapa147@gmail.com>
2025-02-17 21:04:33 +0545

    changed index

--- login.php
+++ login.php
@@ -1,10 +1,11 @@
 <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
-<!DOCTYPE html>
+    GigHub Link: https://github.com/dev-mhrony
+    Facebook Link: https://www.facebook.com/dev.mhrony
+    Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
+    for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
+    Visit My Website : developerrony.com -->
+
+    <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -12,12 +13,10 @@
     <title>Login || Code Camp BD</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
     <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 
     <link rel="stylesheet" href="css/login.css">
-
     <style type="text/css">
     #buttn {
         color: #fff;
@@ -25,27 +24,13 @@
     }
     </style>
 
-    <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
-
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-
 </head>
-<!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
 
 <body>
     <header id="header" class="header-scroll top-header headrom">
@@ -59,117 +44,70 @@
                         <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
 
                         <?php
-						if(empty($_SESSION["user_id"]))
-							{
-								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
-							}
-						else
-							{
-									
-									
-										echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
-									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
-							}
-
-						?>
-                        <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
-
+                            session_start(); 
+                            include("connection/connect.php"); 
+                            if(empty($_SESSION["user_id"])) {
+                                echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+                                      <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+                            } else {
+                                echo '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+                                echo '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+                            }
+                        ?>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
-    <div style=" background-image: url('images/img/pimg.jpg');">
 
+    <div style="background-image: url('images/img/pimg.jpg');">
         <?php
-include("connection/connect.php"); 
-error_reporting(0); 
-session_start(); 
-if(isset($_POST['submit']))  
-{
-	$username = $_POST['username'];  
-	$password = $_POST['password'];
-	
-	if(!empty($_POST["submit"]))   
-     {
-	$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
-	$result=mysqli_query($db, $loginquery); //executing
-	$row=mysqli_fetch_array($result);
-	
-	                        if(is_array($row)) 
-								{
-                                    	$_SESSION["user_id"] = $row['u_id']; 
-										 header("refresh:1;url=index.php"); 
-	                            } 
-							else
-							    {
-                                      	$message = "Invalid Username or Password!"; 
-                                }
-	 }
-	
-	
-}
-?>
+            error_reporting(0);
+            if(isset($_POST['submit'])) {
+                $username = $_POST['username'];  
+                $password = $_POST['password'];
+                
+                if(!empty($_POST["submit"])) {
+                    $loginquery = "SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; 
+                    $result = mysqli_query($db, $loginquery); 
+                    $row = mysqli_fetch_array($result);
+                    
+                    if(is_array($row)) {
+                        $_SESSION["user_id"] = $row['u_id']; 
+                        header("refresh:1;url=index.php"); 
+                    } else {
+                        $message = "Invalid Username or Password!"; 
+                    }
+                }
+            }
+        ?>
 
-        <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
+        <div class="pen-title"></div>
 
-        <div class="pen-title">
-            < </div>
+        <div class="module form-module">
+            <div class="toggle"></div>
+            <div class="form">
+                <h2>Login to your account</h2>
+                <span style="color:red;"><?php echo $message; ?></span>
+                <span style="color:green;"><?php echo $success; ?></span>
+                <form action="" method="post">
+                    <input type="text" placeholder="Username" name="username" />
+                    <input type="password" placeholder="Password" name="password" />
+                    <input type="submit" id="buttn" name="submit" value="Login" />
+                </form>
+            </div>
 
-                <div class="module form-module">
-                    <div class="toggle">
+            <div class="cta">Not registered?<a href="registration.php" style="color:#5c4ac7;"> Create an account</a></div>
+        </div>
+    </div>
 
-                    </div>
-                    <div class="form">
-                        <h2>Login to your account</h2>
-                        <span style="color:red;"><?php echo $message; ?></span>
-                        <span style="color:green;"><?php echo $success; ?></span>
-                        <form action="" method="post">
-                            <input type="text" placeholder="Username" name="username" />
-                            <input type="password" placeholder="Password" name="password" />
-                            <input type="submit" id="buttn" name="submit" value="Login" />
-                        </form>
-                    </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-                    <div class="cta">Not registered?<a href="registration.php" style="color:#5c4ac7;"> Create an account</a></div>
-                </div>
-                <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <div class="container-fluid pt-3">
+        <p></p>
+    </div>
 
-
-                <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
-
-
-                <div class="container-fluid pt-3">
-                    <p></p>
-                </div>
-
-
-
-                <?php include "include/footer.php" ?>
-
-                <!--  Author Name: MH RONY.
-                        GigHub Link: https://github.com/dev-mhrony
-                        Facebook Link:https://www.facebook.com/dev.mhrony
-                        Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-                        for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com  
-                        Visit My Website : developerrony.com -->
-
+    <?php include "include/footer.php" ?>
 
 </body>
 
